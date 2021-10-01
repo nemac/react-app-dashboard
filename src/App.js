8d66834ae640d4443153ae04830673e76daeb4fd
Jeff Bliss <jeff.a.bliss@gmail.com>
2021-10-01 14:20:41 -0400

    super simple update to read from json file and display results in a formatted way

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData=()=>{
+    fetch('data.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! Cool stuff here soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data && data.length>0 && 
+        data.map((item)=>
+          <li key ={item.name.toString()}>
+            <b>Name: </b>{item.name} <b>Profession: </b>{item.profession}
+          </li>
+        )
+      }
     </div>
   );
 }
